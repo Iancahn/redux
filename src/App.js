088449a15fbdb6d372479ecdc52d2bdd7c0e1c86
Ian Cahn <iancahn@live.com>
2022-06-15 17:05:28 +0200

    Weather API working
    So much easier than building own data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+// test
+import React, { useEffect, useState } from 'react';
+import s from './App.css'
+import axios from 'axios';
+
 function App() {
+  const [weather, setWeather] = useState(null);
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    const location = axios.get('http://api.weatherapi.com/v1/current.json?key=d7ab6f7edfc6485fb15141106221506&q=Polokwane&aqi=yes')
+      .then(data => {
+        setWeather(data.data)
+      })
+      .catch(err => console.log(err));
+  }, []);
+  //Event
+  const weatherInput = (e) => {
+    setInput(e.target.value);
+  };
+  const searchWeather = () => {
+    axios.get(`http://api.weatherapi.com/v1/current.json?key=d7ab6f7edfc6485fb15141106221506&q=${input}`)
+      .then(data => {
+        setWeather(data.data);
+      })
+  }
   return (
     <div className="App">
+      {weather && (
+        <div>
+          <div className="search">
+            <input onChange={weatherInput} type="text" />
+            <button onClick={searchWeather} >Search</button>
+            <div>
+              <div className="weather-info">
+                <h1>{weather.location.name}</h1>
+                <h2>{weather.location.region}</h2>
+                <div className="condition">
+                  <h3>The current weather is {weather.current.temp_c}° Celcius</h3>
+                  <img src={weather.current.condition.icon} alt='The current weather condition icon' />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
 
     </div>
   );
-}
+};
 
 export default App;
